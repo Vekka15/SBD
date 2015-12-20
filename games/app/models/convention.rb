e1ac43788832b8b35008d485c26c1addb9d0a62d
Vekka15 <vekka15@gmail.com>
2015-12-20 20:05:48 +0100

    Validacje formatu, rozmiaru, unikalności

--- games/app/models/convention.rb
+++ games/app/models/convention.rb
@@ -1,7 +1,24 @@
 class Convention < ActiveRecord::Base
-  validates_presence_of :name, :message => "Nazwa jest wymagana"
-  validates_presence_of :start, :message => "Czas staru jest wymagany"
-  validates_presence_of :end, :message => "Czas zakończenia jest wymagany"
-  validates_presence_of :country,:message => "Kraj jest wymagany"
-  validates_presence_of :level, :message => "Poziom jest wymagany"
+  #presence
+  validates_presence_of :name, :message => "Nazwa jest wymagana", :allow_nil => true
+  validates_presence_of :start, :message => "Czas startu jest wymagany", :allow_nil => true
+  validates_presence_of :end, :message => "Czas zakończenia jest wymagany", :allow_nil => true
+  validates_presence_of :country,:message => "Kraj jest wymagany", :allow_nil => true
+  validates_presence_of :level, :message => "Poziom jest wymagany", :allow_nil => true
+
+  #format
+  validates_format_of :name, with: /\p{Alpha}/, :message => "Nazwa powinna zawierać litery", :allow_blank => true
+  validates_date :start, :invalid_date_message => "Start musi być datą", :allow_blank => true
+  validates_date :end,:after => :start,:after_message => "Koniec musi być później niż start",:invalid_date_message => "Koniec jest niepoprawne",:allow_blank =>true
+  validates_numericality_of :level,:less_than => 10, :message => "Poziom musi być mniejszy niż 10", :allow_blank => true
+  validates_numericality_of :level, :only_integer => true, :message => "Poziom musi być liczbą", :allow_blank => true
+  validates_format_of :country, with: /\p{Alpha}/, :message => "Państwo musi zawierać litery", :allow_blank => true
+
+  #ograniczenie długości
+  validates_length_of :name, :maximum => 15, :message => "Nazwa jest za długa" , :allow_blank => true
+  validates_length_of :country, :maximum => 15,:message => "Kraj jest za długi", :allow_blank => true
+
+  #unikalność
+  validates_uniqueness_of :name, :message => "Nazwa już istnieje"
+
 end
--- games/app/models/game.rb
+++ games/app/models/game.rb
@@ -1,6 +1,15 @@
 class Game < ActiveRecord::Base
+  has_many :match
+  before_destroy :check_for_match
   validates_presence_of :name, :message => "Nazwa jest wymagana"
   validates_presence_of :number_players, :message => "Liczba graczy jest wymagana"
   validates_presence_of :time, :message => "Czas jest wymagany"
   validates_presence_of :rate, :message => "Ocena jest wymagana"
+
+  def check_for_match
+    unless match.nil?
+      self.errors[:base] << "Nie można usunąć jeżeli istnieją rozgrywki z tą grą."
+      alert("Nie można usunąć jeżeli istnieją rozgrywki z tą grą.");
+    end
+  end
 end
