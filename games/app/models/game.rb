00e1bbdfb0fe778cd46f744574a0202cc80c6ae0
Vekka15 <vekka15@gmail.com>
2015-12-20 22:01:39 +0100

    Validacje gra

--- games/app/models/game.rb
+++ games/app/models/game.rb
@@ -1,10 +1,21 @@
 class Game < ActiveRecord::Base
   has_many :match, :dependent => :restrict_with_error
-  #before_destroy :check_for_match
-  validates_presence_of :name, :message => "Nazwa jest wymagana"
-  validates_presence_of :number_players, :message => "Liczba graczy jest wymagana"
+
+  #presence
+  validates_presence_of :name, :message => "Nazwa jest wymagana", :allow_nil => true
+  validates_presence_of :number_players, :message => "Liczba graczy jest wymagana", :allow_nil => true
   validates_presence_of :time, :message => "Czas jest wymagany"
   validates_presence_of :rate, :message => "Ocena jest wymagana"
 
+  #format
+  validates_format_of :name, :with /\p{Alpha}/, :message => "Nazwa musi zawierać litery", :allow_blank => true
+  validates_numericality_of :number_player,:integer_only => true, :message => "Liczba graczy musi być liczbą", :allow_blank => true
+  validates_numericality_of :number_player,:less_than => 20, :message => "Liczba graczy musi być mniejsza niż 20", :allow_blank => true
+  validates_numericality_of :time, :message => "Czas musi być liczbą", :allow_blank => true #zastanowic sie nad tym
+  validates_numericality_of :rate, :integer_only => true, :message => "Ocena musi być liczbą", :allow_blank => true
+  validates_numericality_of :rate, :less_than => 10, :message => "Ocena musi być mniejsza niż 10", :allow_blank => true
 
+  #unikalność
+  validates_uniqueness_of :name, :message => "Nazwa już istnieje", :allow_blank => true
+  
 end
