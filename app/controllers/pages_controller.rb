8a7d0c2aaaca57e41634d7d21e0362f1f60ba373
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-05 20:49:28 +0100

    test2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def show
-      @stat = ActiveRecord::Base.connection.execute("select staty()")
+      #@stat = ActiveRecord::Base.connection.execute("select staty()")
   end
 end
